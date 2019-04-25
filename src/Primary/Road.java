03be1b842aa31159b91e1324f19a2fe4934b6c0e
Liam Brady <lese100@cs.unm.edu>
2019-04-25 15:53:32 -0600

    road light changes

--- src/Primary/Road.java
+++ src/Primary/Road.java
@@ -6,11 +6,36 @@ public class Road {
 
     private ArrayList<Lanes> lanes = new ArrayList<>();
     private SignalColor lightColor;
+    private final int YELLOW_LIGHT_LENGTH = 1;
 
     public void setRoads(ArrayList<Lanes> lanes){
         this.lanes = lanes;
     }
+    private void waitlength(int seconds){
+        try {
+            Thread.sleep(seconds * 1000);
+        }catch (InterruptedException e){}
+    }
 
+    public void turnLightOn(){
+        // turn lights green
+    }
+
+    public void turnLightOff(){
+        // turn lights yellow
+        waitlength(YELLOW_LIGHT_LENGTH);
+        // turn lights red
+    }
+
+    public void straightLightOn(){
+        // turn lights green
+    }
+
+    public void straightLightOff(){
+        // turn lights yellow
+        waitlength(YELLOW_LIGHT_LENGTH);
+        // turn lights red
+    }
     public ArrayList<Lanes> getRoads(){
         return this.lanes;
     }
